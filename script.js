dc43a75bd1c29fe9805256536843aa4addff0f0b
Michelle Chang <mimichanghighschooler@gmail.com>
2023-03-08 13:28:19 +0800

    update 3.8 上午

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 $('.owl-carousel').owlCarousel({
-  items:2,
+  items:1,
   loop:true,
-  center:true,
-  margin:50,
-  URLhashListener:true,
+  // center:true,
+  margin:10,
+  navText:false,
+  // URLhashListener:true,
   autoplayHoverPause:true,
   smartSpeed:300,
   autoplay:true,
   autoplayTimeout:5000,
   autoplayHoverPause:true,
+  responsive:{
+    576:{
+        items:2,
+        nav:true
+    },
+    768:{
+        items:3,
+        nav:false
+    },
+    1200:{
+        items:4,
+        nav:true,
+        loop:false
+    },
+  //   1400:{
+  //     items:6,
+  //     nav:true,
+  //     loop:false
+  // }
+}
 });
